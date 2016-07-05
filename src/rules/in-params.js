236e7fd700a263a951eef8d4c2dd6c41c2b83081
Luke Page <luke.a.page@gmail.com>
2016-07-05 05:45:42 +0200

    upgrade to eslint v3

--- src/rules/in-params.js
+++ src/rules/in-params.js
@@ -2,38 +2,44 @@
 // Rule Definition
 //------------------------------------------------------------------------------
 
-module.exports = function inParams(context) {
-  const option = context.options[0];
-  let maxParams = 1;
+module.exports = {
+  meta: {
+    docs: {},
 
-  if (typeof option === 'object' &&
-    option.hasOwnProperty('max-params') &&
-    typeof option['max-params'] === 'number') {
-    maxParams = option['max-params'];
-  }
-
-  return {
-    ObjectPattern(node) {
-      if (node.parent.type === 'ArrowFunctionExpression' ||
-      node.parent.type === 'FunctionDeclaration') {
-        if (node.parent.params.length > maxParams) {
-          context.report(node, 'Do not use destructuring in params when there' +
-            ` are more than ${maxParams} params.`);
-        }
-      }
-    },
-  };
-};
-
-module.exports.schema = [
-  {
-    type: 'object',
-    properties: {
-      'max-params': {
-        type: 'integer',
-        minimum: 0,
+    schema: [
+      {
+        type: 'object',
+        properties: {
+          'max-params': {
+            type: 'integer',
+            minimum: 0,
+          },
+        },
+        additionalProperties: false,
       },
-    },
-    additionalProperties: false,
+    ],
   },
-];
+
+  create: function inParams(context) {
+    const option = context.options[0];
+    let maxParams = 1;
+
+    if (typeof option === 'object' &&
+      option.hasOwnProperty('max-params') &&
+      typeof option['max-params'] === 'number') {
+      maxParams = option['max-params'];
+    }
+
+    return {
+      ObjectPattern(node) {
+        if (node.parent.type === 'ArrowFunctionExpression' ||
+        node.parent.type === 'FunctionDeclaration') {
+          if (node.parent.params.length > maxParams) {
+            context.report(node, 'Do not use destructuring in params when there' +
+              ` are more than ${maxParams} params.`);
+          }
+        }
+      },
+    };
+  },
+};
--- src/rules/no-rename.js
+++ src/rules/no-rename.js
@@ -2,15 +2,21 @@
 // Rule Definition
 //------------------------------------------------------------------------------
 
-module.exports = function noRename(context) {
-  return {
-    Property(node) {
-      if (node.parent.type === 'ObjectPattern' &&
-        node.shorthand === false &&
-        node.value &&
-        node.value.type === 'Identifier') {
-        context.report(node, 'Do not use destructuring rename.');
-      }
-    },
-  };
+module.exports = {
+  meta: {
+    docs: {},
+  },
+
+  create: function noRename(context) {
+    return {
+      Property(node) {
+        if (node.parent.type === 'ObjectPattern' &&
+          node.shorthand === false &&
+          node.value &&
+          node.value.type === 'Identifier') {
+          context.report(node, 'Do not use destructuring rename.');
+        }
+      },
+    };
+  },
 };
